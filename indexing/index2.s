5a579edf4bcb921bd306d8775ee10fb8007f72de
Phil Peterson <phpeterson@usfca.edu>
2020-02-26 01:07:55 +0000

    fix whitespace on the comments too

--- indexing/index2.s
+++ indexing/index2.s
@@ -1,5 +1,5 @@
 /*
- * indexing_shift.s - Demonstrate ARM indexing mode using shifted register
+ * index2.s - Demonstrate ARM indexing mode using shifted register
  */
 
 .global index2
@@ -8,12 +8,12 @@
 @ r1: count of words in the buffer
 
 index2:
-    mov ip, #0						@ r12 (aka ip)  is the loop variable
+    mov ip, #0                      @ r12 (aka ip)  is the loop variable
 loop:
-    ldr r2, [r0, ip, LSL #2]		@ r2 = *(r0 + ip*4)
-    add r2, r2, #1					@ r2++
-    str r2, [r0, ip, LSL #2]		@ *(r0 + ip*4) = r2
-    add ip, ip, #1					@ loop++
-    cmp ip, r1						@ done?
-    blt loop						@	not yet, go again
-    bx lr							@ return
+    ldr r2, [r0, ip, LSL #2]        @ r2 = *(r0 + ip*4)
+    add r2, r2, #1                  @ r2++
+    str r2, [r0, ip, LSL #2]        @ *(r0 + ip*4) = r2
+    add ip, ip, #1                  @ loop++
+    cmp ip, r1                      @ done?
+    blt loop                        @   not yet, go again
+    bx lr                           @ return
