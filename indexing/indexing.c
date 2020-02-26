5a556ae42559a8c0e4088a20c813f31e5e2b854e
Phil Peterson <phpeterson@usfca.edu>
2020-02-26 01:03:42 +0000

    hopefully fix whitespace by changing setexpandtab in .vimrc again. I had made it noexpandtab when we were working with makefiles

--- indexing/indexing.c
+++ indexing/indexing.c
@@ -4,21 +4,21 @@ void index1(int *, int);
 void index2(int *, int);
 
 void print_arr(int *arr, int len) {
-	for (int i = 0; i < len; i++)
-		printf("%d ", arr[i]);
-	printf("\n");
+    for (int i = 0; i < len; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
 }
 
 int main(int argc, char **argv) {
-	#define COUNT 8
-	int arr[COUNT] = {0,1,2,3,4,5,6,7};
-	int i;
+    #define COUNT 8
+    int arr[COUNT] = {0,1,2,3,4,5,6,7};
+    int i;
 
-	index1(arr, COUNT);
-	print_arr(arr, COUNT);
+    index1(arr, COUNT);
+    print_arr(arr, COUNT);
 
-	index2(arr, COUNT);
-	print_arr(arr, COUNT);
+    index2(arr, COUNT);
+    print_arr(arr, COUNT);
 
-	return 0;
+    return 0;
 }
