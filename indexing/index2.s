7c478118ba06f03c000757d7f3460c1baadb5311
Phil Peterson <phpeterson@usfca.edu>
2020-02-26 00:56:53 +0000

    add shifting example

---
+++ indexing/index2.s
@@ -0,0 +1,21 @@
+/*
+ * indexing_shift.s - Demonstrate ARM indexing mode using shifted register
+ */
+
+.global index2
+
+@ r0: memory buffer of words (32 bits each)
+@ r1: count of words in the buffer
+
+index2:
+	mov ip, #0						@ r12 (aka ip)  is the loop variable
+loop:
+	ldr r2, [r0, ip, LSL #2]		@ r2 = *(r0 + ip*4)
+	add r2, r2, #1					@ r2++
+	str r2, [r0, ip, LSL #2]		@ *(r0 + ip*4) = r2
+	add ip, ip, #1					@ loop++
+	cmp ip, r1						@ done?
+	blt loop						@	not yet, go again
+	bx lr							@ return
+
+
--- indexing/indexing.c
+++ indexing/indexing.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 
 void index1(int *, int);
+void index2(int *, int);
+
+void print_arr(int *arr, int len) {
+	for (int i = 0; i < len; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+}
 
 int main(int argc, char **argv) {
-	int arr[8] = {0,1,2,3,4,5,6,7};
-	index1(arr, 8);
+	#define COUNT 8
+	int arr[COUNT] = {0,1,2,3,4,5,6,7};
+	int i;
+
+	index1(arr, COUNT);
+	print_arr(arr, COUNT);
+
+	index2(arr, COUNT);
+	print_arr(arr, COUNT);
+
 	return 0;
 }
