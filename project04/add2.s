a00535dba47bb6b98e0250e2fbec2015c4320cdf
Phil Peterson <phpeterson@usfca.edu>
2020-03-26 21:29:40 +0000

    code from 3/26 lecture cleaned up

--- project04/add2.s
+++ project04/add2.s
@@ -1,6 +1,7 @@
 .global add2
 
 add2:
+    sub r0, r7, r8
     add r0, r1, r2
     bx lr
     
--- project04/main.c
+++ project04/main.c
@@ -1,30 +1,49 @@
 #include <stdio.h>
+#include <string.h>
+
+#include "armemu.h"
 
 int add2(int, int);
 
 int main(int argc, char **argv) {
 
-    unsigned int registers[16]; // emulated registers
+    // Initialize emulator
+    emulator emu;
+    memset(emu.regs, 0, sizeof(unsigned int));
+    emu.cpsr = 0;
 
-    // fetch
-    unsigned int *ptr_to_instructions = (unsigned int*) add2;
-    unsigned int iw = *ptr_to_instructions; // instruction word
+    // PC <- pointer to instructions as an unsigned int
+    emu.regs[REG_PC] = (unsigned int) add2;
 
-    // decode
+    do {
+        unsigned int iw = *(unsigned int*) emu.regs[REG_PC];
 
-    // destination reg is 4 bits starting at bit 12
-    unsigned int rd = (iw >> 12) & 0b1111;
+        // Assume it's a data processing instruction in this starter code.
+        // Your real code will decode iw according to the bits shown in the
+        // instruction set manual I posted
 
-    // first source reg is 4 bits starting at bit 16
-    unsigned int rn = (iw >> 16) & 0b1111;
+        // destination reg is 4 bits starting at bit 12
+        unsigned int rd = (iw >> 12) & 0b1111;
 
-    // opcode (of the data processing family) is 4 bits starting at bit 21
-    unsigned int opcode = (iw >> 21) & 0b1111;
-    
-    if (opcode == 0b0100) { 
-        // execute ADD instruction
-        registers[rd] = registers[rn] + 0; // next time: + decode op2
-    }
+        // first source reg is 4 bits starting at bit 16
+        unsigned int rn = (iw >> 16) & 0b1111;
 
-    printf("result: %d\n", rd);
+        // opcode (of the data processing family) is 4 bits starting at bit 21
+        unsigned int opcode = (iw >> 21) & 0b1111;
+
+        if (opcode == 0b0100) { 
+            // execute ADD instruction
+            emu.regs[rd] = emu.regs[rn] + 0; // need to decode op2
+        } else if (opcode == 0b0010) {
+            // execute SUB instruction
+            emu.regs[rd] = emu.regs[rn] - 0; // need to decode op2
+        }
+
+        // Move PC forward 4 bytes to get the next instruction
+        emu.regs[REG_PC] += 4;
+        
+    } while (1); // Your real code will have a way to stop
+
+    // Return value is in R0, just like AAPCS
+    printf("result: %d\n", emu.regs[0]);
 }
